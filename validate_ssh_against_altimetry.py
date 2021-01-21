f79646bf93f28e50ad786c8bf5a4017e32077d69
david <dbyrne@noc.ac.uk>
2021-01-21 10:30:46 +0000

    Tidal validation almost complete

---
+++ validate_ssh_against_altimetry.py
@@ -0,0 +1,5 @@
+"""
+Created on Mon Jan 18 16:13:45 2021
+
+@author: Dave
+"""
---
+++ validate_ssh_against_tide_gauge.py
@@ -0,0 +1,41 @@
+"""
+@author: Dave (dbyrne@noc.ac.uk)
+"""
+
+fn_nemo_harmonics = ""
+fn_nemo_domain    = ""
+
+dn_tidal_harmonics = ""
+
+dn_output = ""
+
+constituents = []
+
+
+
+
+
+##############################################################################
+
+import coast
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print('Main')
+
+
+def read_harmonic_obs(path, ):
+    return
+
+def plot_harmonics_on_map():
+    return
+
+def plot_stats_on_map():
+    return
+
+def write_stats_to_file():
+    return
+
+if __name__ == '__main__':
+    main()
